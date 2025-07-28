obj_plan rotation corrected, camera string updated

camera.py
@@ -74,8 +74,9 @@ class Camera:
         return glm.perspective(glm.radians(FOV), self.aspect_ratio, NEAR, FAR)
     
     def get_camera_str(self):
-        return f'p: ({self.position.x:.1f}, {self.position.y:.1f}, {self.position.z:.1f}), yaw: {self.yaw:.0f}, pitch: {self.pitch:.0f}'
-    
+        return f'p: ({self.position.x:.2f}, {self.position.y:.1f}, {self.position.z:.1f}), '\
+               f'yaw: {self.yaw:.0f}deg {np.radians(self.yaw):.2f}rad, pitch: {self.pitch:.0f}deg {np.radians(self.pitch):.2f}rad'
+
     def get_camera_interp_data(self):
 
         positions = np.array([ # t,x,y,z

model.py
@@ -79,8 +79,8 @@ class DefaultOBJ(BaseModel):
                  path_obj='objects/terrain/terrain.obj', path_texture='objects/terrain/terrain.png',
                  path:np.ndarray=None, rotation_available:bool=False, 
                  normalize_dimensions:bool=False, center_obj:bool=False,
-                 pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
-        
+                 pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1), **kwargs):
+
         # Center the obj file before loading it to the vbo
         if center_obj and not vbo_name in app.mesh.vao.vbo.vbos:
             if (Path(__file__).parent/path_obj).with_suffix('.obj.bin').exists():
@@ -106,6 +106,7 @@ class DefaultOBJ(BaseModel):
     
         self.path = path
         self.rot_available = rotation_available
+        self.kwargs = kwargs
 
         super().__init__(app, vao_name, tex_id, pos, rot, scale)
         self.update_initial_transform_parameters_for_dimension_normalization(vbo_name, normalize_dimensions)
@@ -115,18 +116,18 @@ class DefaultOBJ(BaseModel):
         self.update_model_transform()
 
         # texture
-        self.program['alpha'] = 1
+        self.program['alpha'] = self.kwargs['alpha'] if 'alpha' in self.kwargs else 1.0
         self.texture = self.app.mesh.texture.textures[self.tex_id]
         self.program['u_texture_0'] = 0
         self.texture.use(location = 0)
 
         self.update_uniform_transformation_matrices(update_proj=True)
         self.update_uniform_light()
-        self.program['light.Ia'].write(self.app.light.Ia+0.2)
 
     def update(self):
         self.update_model_transform()
         self.update_uniform_transformation_matrices(update_camPos=True)
+        self.program['alpha'] = self.kwargs['alpha'] if 'alpha' in self.kwargs else 1.0
         self.texture.use()
         
         self.app.info_display.update_info_section(self.tex_id,f'{self.tex_id} pos: {self.pos}, rot: {self.rot}, scale: {self.scale}')

scene.py
@@ -61,7 +61,8 @@ class Scene:
                                             rotation_available=True,
                                             normalize_dimensions=True,
                                             center_obj=True,
-                                            scale=2*obj_plan['dimension']/np.max(obj_plan['world_dimensions'])))
+                                            scale=2*obj_plan['dimension']/np.max(obj_plan['world_dimensions']),
+                                            alpha=obj_plan['color'][3]))
         '''
         self.add_object(DefaultOBJ(app, vao_name='cat', vbo_name='cat', tex_id='cat',
                                     path_obj='objects/cat/20430_Cat_v1_NEW.obj',

utils.py
@@ -264,15 +264,13 @@ def get_demo_data_for_obj_plans():
 
     path1 = np.load(folder/'back_and_forth_trajectory_1.npy')
     path2 = np.load(folder/'ellipsoidal_trajectory_map_centered.npy')
-    path3 = np.load(folder/'ellipsoidal_trajectory_origin.npy')
+    path3 = np.load(folder/'ellipsoidal_trajectory_map_origin.npy')
     world_dim = np.array([400e3, 400e3, 6e3], dtype=np.float32)
 
-    path1[:,4:6] = 0
-    path1[:,6] = np.linspace(0, 2*np.pi, path1.shape[0])  # Add yaw rotation
-
-    path2[:,4] = np.linspace(0, 2*np.pi, path2.shape[0])  # Add yaw rotation
-    path2[:,5] = np.linspace(0, 2*np.pi, path2.shape[0])  # Add yaw rotation
-    path2[:,6] = np.linspace(0, 2*np.pi, path2.shape[0])  # Add yaw rotation
+    # DEBUG
+    #path3[:,6] = np.linspace(0, 2*np.pi, path3.shape[0])
+    #path3[:,4] = 0
+    #path3[:,5] = 0
 
     obj_plans = [{'id':'radar_0', 'type':'radar', 'path':path1,'color':(0,255,255,0.5),'world_dimensions':world_dim, 'dimension':80e3},
                  {'id':'cone_0', 'type':'cone', 'path':path2,'color':(255,255,0,0.5),'world_dimensions':world_dim, 'dimension':60e3},