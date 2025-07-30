Corrected dimension normalization with multiple options. GE can now accept kwargs, not just cmd arguments.

data.py
@@ -9,9 +9,9 @@ class Data:
     def __init__(self, app):
         self.app = app
         try:
-            self.folder = Path(app.args.folder)
+            self.folder = Path(app.config['folder'])
         except:
-            print(f'Invalid folder path: {self.folder}. Please provide a valid path.')
+            print(f'Invalid folder path: {app.config['folder']}. Please provide a valid path.')
 
         # grid_seq: np.ndarray with indices: time,x,y,z
         # plans: list of dictionaries with keys: 'path_extracted' ,'path_corrected' ,'path_interp_BSpline', 'path_interp_MinimumSnapTrajectory', etc.
@@ -25,7 +25,7 @@ class Data:
             'terrain': self.load_terrain
         }
 
-        self.scene = app.args.scene if isinstance(app.args.scene, list) else [app.args.scene]
+        self.scene = app.config['scene'] if isinstance(app.config['scene'], list) else [app.config['scene']]
         if 'all' in self.scene:
             self.scene = list(load_func_dict.keys())
         for key in load_func_dict:
@@ -150,6 +150,8 @@ class Data:
         # Iterate through the object plans and convert the paths to the correct format
         for obj_plan in self.obj_plans:
 
+            print(f"Object plan {obj_plan['id']} loaded, type: {obj_plan['type']}, path shape: {obj_plan['path'].shape} start: {obj_plan['path'][0,1:4]}, dimension: {obj_plan['dimension']}, world dimensions: {obj_plan['world_dimensions']}")
+
             if np.issubdtype(obj_plan['path'].dtype, np.floating):
                 print('Path is of type float, time is already in seconds, path is in SI units, no conversion needed')
 
@@ -164,13 +166,16 @@ class Data:
                 obj_plan['path'][:,0] = obj_plan['path'][:,0] / self.app.clock.FPS_animation  # Convert time to seconds
                 obj_plan['path'][:,1:4] = obj_plan['path'][:,1:4] / np.max(self.world_dimensions).astype(np.float32)  # Normalize the position, by the largest dimension of the world
 
+
+            # Transform the path to the correct format (OpenGL expects Y and Z to be swapped, and X to be flipped)
             obj_plan['path'][:,1] = -obj_plan['path'][:,1]  # Flip x axis
             obj_plan['path'][:,[2,3]] = obj_plan['path'][:,[3,2]]  # Swap y and z
             if obj_plan['path'].shape[1] >= 7: # Rotation information on indices 4,5,6
                 obj_plan['path'][:,4] = - obj_plan['path'][:,4]  # Flip x axis
                 obj_plan['path'][:,[5,6]] = obj_plan['path'][:,[6,5]]  # Swap y and z
-
-            print(f"Object plan {obj_plan['id']} loaded: path shape: {obj_plan['path'].shape} start: {obj_plan['path'][0,1:4]}, dimension: {obj_plan['dimension']}, world dimensions: {obj_plan['world_dimensions']}")
+            if isinstance(obj_plan['dimension'], (tuple, list, np.ndarray)):
+                obj_plan['dimension'] = np.array(obj_plan['dimension'])
+                obj_plan['dimension'][[1, 2]] = obj_plan['dimension'][[2, 1]]  # Swap y and z dimensions
 
         # If you want to add a custom object .obj file created by an external program,
         # first open the obj file and comment out lines starting with: 'mtllib' and 'usemtl'

model.py
@@ -50,21 +50,28 @@ class BaseModel:
         if update_proj: self.program['m_proj'].write(self.camera.m_proj)
         if update_camPos: self.program['camPos'].write(self.camera.position)
 
-    def set_offset_parameters_for_dimension_normalization(self, vertex_data):        
-        vertex_data = vertex_data.reshape(-1, 3)    # Reshape the vertex data into a (N, 3) array
-        min_coords = np.min(vertex_data, axis=0)    # Calculate the min and max for each axis (x, y, z)
-        max_coords = np.max(vertex_data, axis=0)
+    def set_offset_parameters_for_dimension_normalization(self, vertex_data, mode='max'):
+        """ Set the offset scale for dimension normalization based on vertex data. """
+        if mode in ['max', 'unit']:
+            vertex_data = vertex_data.reshape(-1, 8)         # Reshape the vertex data into a (N, 8) array, format: [texcoord, normal, position]
+            vertex_positions = vertex_data[:, 5:8]           # Extract the position data
+            min_coords = np.min(vertex_positions, axis=0)    # Calculate the min and max for each axis (x, y, z)
+            max_coords = np.max(vertex_positions, axis=0)
+            bbox = max_coords - min_coords                  # Bounding box dimensions
+            if mode == 'max':
+                self.offset_scale = float(1 / np.max(bbox)) # Keep aspect ratio, divide by the largest dimension
+            elif mode == 'unit':
+                self.offset_scale = 1 / bbox                # Rescale to unit cube
+        else:
+            raise ValueError("Invalid mode for dimension normalization. Use 'max' or 'unit'.")
 
-        self.offset_scale = float(1 / np.max(max_coords - min_coords))  # Scale factor to fit within a unit cube
-
-    def update_initial_transform_parameters_for_dimension_normalization(self, vbo_name, normalize_dimensions=False):
-        if normalize_dimensions:
-            # set scale for dimension normalization
-            self.set_offset_parameters_for_dimension_normalization(self.app.mesh.vao.vbo.vbos[vbo_name].vertex_data)
+    def update_initial_transform_parameters_for_dimension_normalization(self, vbo_name):
+        if 'normalize_dimensions' in self.kwargs:
+            self.set_offset_parameters_for_dimension_normalization(self.app.mesh.vao.vbo.vbos[vbo_name].vertex_data, mode=self.kwargs['normalize_dimensions'])
         else:
             self.offset_scale = 1
 
-        self.initial_scale = glm.vec3(tuple(np.broadcast_to(self.offset_scale * np.array(self.initial_scale), 3).astype(float)))
+        self.initial_scale = glm.vec3(tuple(np.broadcast_to(np.array(self.offset_scale) * np.array(self.initial_scale), 3).astype(float)))
 
         self.pos = glm.vec3(self.initial_pos)
         self.rot = glm.vec3(self.initial_rot)
@@ -77,12 +84,16 @@ class BaseModel:
 class DefaultOBJ(BaseModel):
     def __init__(self, app, vao_name='default', vbo_name='default', tex_id='test', 
                  path_obj='objects/terrain/terrain.obj', path_texture='objects/terrain/terrain.png',
-                 path:np.ndarray=None, rotation_available:bool=False, 
-                 normalize_dimensions:bool=False, center_obj:bool=False,
+                 path:np.ndarray=None, rotation_available:bool=False,
                  pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1), **kwargs):
+        '''kwargs:
+        - alpha: float, default 1.0, transparency of the object
+        - normalize_dimensions: str, 'max' or 'unit', default 'None', how to normalize the dimensions of the object
+        - center_obj: bool, default False, whether to center the object (using its bounding box) before loading it to the vbo
+        '''
 
         # Center the obj file before loading it to the vbo
-        if center_obj and not vbo_name in app.mesh.vao.vbo.vbos:
+        if 'center_obj' in kwargs and kwargs['center_obj'] and not vbo_name in app.mesh.vao.vbo.vbos:
             if (Path(__file__).parent/path_obj).with_suffix('.obj.bin').exists():
                 print('The OBJ file centering is only executed if a new .obj file is added or updated. ' \
                       'Delete the cache files if the .obj file is updated.')
@@ -110,7 +121,7 @@ class DefaultOBJ(BaseModel):
         self.kwargs = kwargs
 
         super().__init__(app, vao_name, tex_id, pos, rot, scale)
-        self.update_initial_transform_parameters_for_dimension_normalization(vbo_name, normalize_dimensions)
+        self.update_initial_transform_parameters_for_dimension_normalization(vbo_name)
         self.on_init()
 
     def on_init(self):

scene.py
@@ -50,7 +50,7 @@ class Scene:
                                     path_texture='objects/terrain/terrain.png',
                                     rot=rot, scale=1))
 
-        if 'obj' in self.scene:  
+        if 'obj' in self.scene:
             for obj_plan in self.app.data.obj_plans:
                 if obj_plan['type'] == 'drone':
                     tex_id = 'drone' # same texture for all drones
@@ -64,19 +64,24 @@ class Scene:
                     self.app.mesh.texture.textures[obj_plan['id']] = create_texture_from_rgba(self.app.ctx, rgba=obj_plan['color'])
                     tex_id = obj_plan['id'] # separete texture for each object
                     path_texture = None
-                    rot=(0,0,0) # Possibly no rotation needed for other objects, but need to check with an object that has a detectable orientation in Z
+                    rot=(0,0,0) # Possibly no rotation needed for other objects
+
+                    if isinstance(obj_plan['dimension'], (float, int)): # If dimension is a single value, normalize dimensions with keeping aspect ratio
+                        normalize_dimensions = 'max' # normalize by the largest dimension of the instance
+                    else:
+                        normalize_dimensions = 'unit' # rescale to unit cube
 
                 self.add_object(DefaultOBJ(app, vao_name=obj_plan['id'],
                                             vbo_name=obj_plan['type'],
                                             tex_id= tex_id,
                                             path_obj=f'objects/obj/{obj_plan["type"]}.obj',
                                             path_texture=path_texture,
-                                            path=obj_plan['path'], 
+                                            path=obj_plan['path'],
                                             rotation_available=True,
-                                            normalize_dimensions=True,
+                                            normalize_dimensions=normalize_dimensions,
                                             center_obj=False,
                                             rot=rot,
-                                            scale=2*obj_plan['dimension']/np.max(obj_plan['world_dimensions']),
+                                            scale=2*np.array(obj_plan['dimension'])/np.max(obj_plan['world_dimensions']),
                                             alpha=obj_plan['color'][3]))
              
         '''self.add_object(DefaultOBJ(app, vao_name='cat', vbo_name='cat', tex_id='cat', path_obj='objects/cat/20430_Cat_v1_NEW.obj',

utils.py
@@ -267,7 +267,7 @@ def get_demo_data_for_obj_plans():
     path2 = np.load(folder/'ellipsoidal_trajectory_map_centered.npy')
     path3 = np.load(folder/'ellipsoidal_trajectory_map_origin.npy')
     path4 = np.load(folder/'drone.npy')
-    world_dim = np.array([400e3, 400e3, 6e3], dtype=np.float32)
+    world_dim = np.array([400e3, 400e3, 6e3])
 
     # DEBUG
     #path4[:,0] -= path4[0,0]
@@ -283,8 +283,8 @@ def get_demo_data_for_obj_plans():
     plt.tight_layout()
     plt.show()
     
-
     obj_plans = [{'id':'radar_0', 'type':'radar', 'path':path1,'color':(255,0,0,0.5),'world_dimensions':world_dim, 'dimension':80e3},
+                 {'id':'radar_1', 'type':'radar', 'path':np.zeros_like(path1),'color':(255,0,0,0.5),'world_dimensions':world_dim, 'dimension':[50e3,5e3,30e3]},
                  {'id':'cone_0', 'type':'cone', 'path':path2,'color':(0,255,0,0.5),'world_dimensions':world_dim, 'dimension':60e3},
                  {'id':'torus_0', 'type':'torus', 'path':path3,'color':(0,0,255,0.5),'world_dimensions':world_dim, 'dimension':40e3},
                  {'id':'drone_0', 'type':'drone', 'path':path4,'color':(0,0,0,1.0),'world_dimensions':world_dim, 'dimension':20e3}]